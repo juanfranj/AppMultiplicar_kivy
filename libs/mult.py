1de4c7728aeb47b4cc349bb59426cf9daaaabbfb
Juanfran <juanfranj@hotmail.com>
2022-04-07 11:45:26 +0200

    multiplicaciones

--- libs/mult.py
+++ libs/mult.py
@@ -2,95 +2,105 @@ from time import sleep
 from random import randint, shuffle
 from bd.funcionesBD import *
 import os
-import threading
+from threading import Thread
 
 
-def continuar(pasar):
-    while not pasar.get():
-        #print("pasar: ", pasar.get())
-        sleep(0.1)
-    #print("Pasar Actualizado: ", pasar.get())
 
-def comenzar(total, texto, resultado, pasar, texto_multi, mul, tablas, repaso):
-    t1 = threading.Thread(target =   multiplicar, args = (total,texto, resultado, pasar, texto_multi, mul, tablas, repaso), daemon = True)
+def comenzar(total, texto, resultado, pasar, texto_multi, mul, tablas, repaso, semaforo):
+    t1 = Thread(target =   multiplicar, args = (total,texto, resultado, pasar, texto_multi, mul, tablas, repaso, semaforo), daemon = True)
     t1.start()
+    
 
-def multiplicar(total, texto, resultado, pasar, texto_multi, multi, tablas_chk, repaso):
+def multiplicar(total, texto, resultado, pasar, texto_multi, multi, tablas_chk, repaso, semaforo):
     tab = tablas_chk
+    #texto.text = "entrando en el programa de multiplicar"
     #print(tablas_chk)
     #tablas = [2,3,4,5]
     tablas = [i+2 for i in range(len(tablas_chk)) if tablas_chk[i] is True]
     #print(tablas)
     try:
         if repaso:
-            total.set(numero_errores())
-        num = total.get()
+            #print("El numero de errores es: ", numero_errores())
+            num = numero_errores()
+        else:
+            num = int(total.text)
+        #print("numero de multiplicaciones: ", num)
     except:
         num = 0
 
-    texto.set("")
+    texto.text = ""
     mul = 1
     error = 0
     errores = 0
     while mul <= num:
         try:
             a, b = numeros_random(tablas, repaso)
-            texto_multi.set(f"{a}x{b}")
-            multi.set(f"Multiplicaciones: {mul}")
+            texto_multi.text = (f"{a}x{b}")
+            #print(f"funcion multiplicar texto_mlti: {texto_multi} id: {id(texto_multi)}")
+            
+            multi.text = (f"Nº Multiplicaciones: {num}     Multiplicaciones: {mul}")
             #cmd = resultado.bind("<Return>",resultado.get())
             #cmd = int(resultado.get())
             fin_cuenta = False
             while not fin_cuenta:
-                texto.set("")
-                continuar(pasar)
-                if int(resultado.get()) == a * b:
-                    modificar_valor(f"{a}x{b}", True)
-                    texto.set("¡¡Bien!!")
+                texto.text = ""
+                #continuar(pasar)
+                #print("semaforo rojo")
+                semaforo.acquire()
+                
+                if int(resultado.text) == a * b:
+                    #print("El resultado es: ", resultado.text)
+                    #modificar_valor(f"{a}x{b}", True)
+                    texto.text = "¡¡Bien!!"
                     sleep(1)
                     fin_cuenta = True
                     mul += 1
                     error = 0
                  
                 else:
-                    modificar_valor(f"{a}x{b}", False)
-                    path = os.getcwd()+ "\\files\errores.txt"
+                    #modificar_valor(f"{a}x{b}", False)
+                    path = os.getcwd()+ "\\AppMulti\\files\errores.txt"
                     escribir_fichero(f"{a}x{b}", path)
                     errores += 1
                     if error == 1:
-                        texto.set("Dejate de rollo Carmen y no la cagues más")
+                        texto.text = "Dejate de rollo y no la cagues más"
                         sleep(1)
                         error -= 1
                     else:
-                        texto.set(f"Lo siento te has equivocado, intentalo otra vez")
+                        texto.text = f"Lo siento te has equivocado, intentalo otra vez"
                         sleep(1)
                         error += 1
-                pasar.set(False)
-                resultado.set("")
+                #pasar.set(False)
+                #print("nueva multiplicacion")
+                #resultado.hint_text = "Ingresar Valor"
         
                    
         except:
-            texto.set("Dejate de rollo Carmen y sigue multiplicando")
+            texto.text = "Dejate de rollo y sigue multiplicando"
             sleep(1)
-            pasar.set(False)
-            resultado.set("")
+            pasar = False
+            #resultado.hint_text = "Ingresar Valor"
 
     modificar_archivo()    
-    total.set("")
+    #iniciar_total()
+
     if num > 0:
-        texto_multi.set(f"Errores: {errores}")
-        texto.set(f"Multiplicaciones: {num}")
+        texto_multi.text = f"Errores: {errores}"
+        texto.text = f"Multiplicaciones: {num}"
     else:
         if not repaso:
-            texto_multi.set(f"Introduce número de")
-            texto.set(f"Multiplicaciones")
+            texto_multi.text = f"Introduce número de"
+            texto.text =f"Multiplicaciones"
+            multi.text = ""
         else:
-            texto_multi.set(f"No existen errores")
-            texto.set(f"para repasar")
+            texto_multi.text = f"No existen errores"
+            texto.text = f"para repasar"
+            multi.text = ""
     
-    sleep(4)
-    texto_multi.set(f" ")
-    texto.set("Hola Carmen, ¿preparada para repasar las tablas?")
-    multi.set(f" ")
+    sleep(3)
+    texto_multi.text = f" "
+    multi.text = "¿Preparad@ para repasar las tablas?"
+    texto.text = f" "
 
 
 def numeros_random(tablas, repaso):
@@ -105,7 +115,8 @@ def numeros_random(tablas, repaso):
     return a, b
 
 def seleccionar_error():
-    path = os.getcwd()+ "\\files\errores.txt"
+    path = os.getcwd()+ "\\AppMulti\\files\errores.txt"
+    #print("Buscando error en: ", path)
     file = open(path, "r")
     errores = file.read().split("//")[:-1]
     #print(errores)
@@ -128,7 +139,8 @@ def escribir_fichero(string, path):
     file.close()
 
 def modificar_archivo():
-    path = os.getcwd()+ "\\files\errores.txt"
+    path = os.getcwd()+ "\\AppMulti\\files\errores.txt"
+    #print("error path: ", path)
     file = open(path, "r")
     errores_brutos = file.read().split("//")[:-1]
     errores_netos = {i for i in errores_brutos}
@@ -139,7 +151,7 @@ def modificar_archivo():
     file.close()
 
 def numero_errores():
-    path = os.getcwd()+ "\\files\errores.txt"
+    path = os.getcwd()+ "\\AppMulti\\files\errores.txt"
     file = open(path, "r")
     errores = file.read().split("//")[:-1]
     file.close()
