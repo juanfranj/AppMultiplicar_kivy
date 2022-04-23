d4f6b1a16507cab0883c53b08d674830a805f2a8
Juanfran <juanfranj@hotmail.com>
2022-04-23 13:30:28 +0200

    sumar

--- libs/sumar.py
+++ libs/sumar.py
@@ -1,8 +1,9 @@
-from turtle import width
+
 from kivy.uix.screenmanager import Screen
 from kivy.uix.gridlayout import GridLayout
 from kivy.uix.label import Label
 from kivymd.uix.button import MDFillRoundFlatButton
+from kivymd.uix.textfield import MDTextField
  
 
 from kivymd.app import MDApp
@@ -49,28 +50,19 @@ class Sumar(Screen):
         #imprimir_pregunta(self.matriz, self.num_fil)
         #imprimir_resultado(self.matriz, self.num_fil)
         self.texto_ayuda.text = f"SUMAR"  
-        self.texto_ayuda.haling = "center"           
+        #self.texto_ayuda.haling = "center"           
     
     def mostrar_resultado(self):
         
-        
-        self.comprobar_celdas()
-        
-        #self.texto_ayuda = self.ids["informacion"]
-        #self.sumandos = self.ids["sumandos"]
-        #self.digitos = self.ids["digitos"]
-        #self.datos_entrada()
-        #self.numeros = devolver_sumandos(self.int_sumandos, self.int_digitos)
-        #self.matriz, self.num_fil, self.num_col = suma(self.numeros, self.int_digitos)
-        
-        #self.grid = self.ids["grid"]
-        #self.grid.cols = 6
-        #self.grid.rows = 8
-         
-        self.escribir_resultado(self.int_digitos)
-        
-        self.texto_ayuda.text = f"RESULTADO"  
-        self.texto_ayuda.haling = "center" 
+        try:
+            self.comprobar_celdas() 
+            self.escribir_resultado(self.int_digitos)
+            self.texto_ayuda.text = f"RESULTADO"  
+            self.texto_ayuda.haling = "center" 
+        except:
+            self.texto_ayuda = self.ids["informacion"]
+            self.texto_ayuda.text = f"Pulsa Botón Calcular"  
+            self.texto_ayuda.haling = "center" 
              
 
 
@@ -124,10 +116,23 @@ class Sumar(Screen):
 
     def escribir_sumandos(self, mas):
         #print("el numero de filas es: ", self.num_fil)
-        for i in range(0,self.num_fil-1):
+        for i in range(0,self.num_fil):
             for j in range(0,self.num_col):
-                if i == 0:
-                    self.texto = Label(text = f"")
+                if (i == 0 and j >= (5-mas)) or (i == self.num_fil-1 and j >= (5-mas)):
+                    #self.texto = Label(text = f"")
+                    self.texto = MDTextField(
+                        #hint_text = "1..9",
+                        input_type = "number",
+                        halign ="center",
+                        #font_size = "25",
+                        #color_mode = 'custom',
+                        text_color = (1.0, 1.0, 1.0, 1)
+                        #foreground_color = (.0, .0, .0, 1)
+                    )
+                    if i == 0:
+                        self.texto.font_size = '25'
+                    else:
+                        self.texto.font_size = '35'
                     self.celdas.append(self.texto)
                     self.grid.add_widget(self.texto)
 
@@ -165,7 +170,7 @@ class Sumar(Screen):
                 self.int_sumandos = int(self.sumandos.text)
         except:
             self.int_sumandos = 3
-            self.texto_ayuda.text = f"Sumandos_error: {self.int_sumandos} Digitos: {self.digitos.text}"
+            #self.texto_ayuda.text = f"Sumandos_error: {self.int_sumandos} Digitos: {self.digitos.text}"
         
         #calculo de digitos
         try:
@@ -175,7 +180,7 @@ class Sumar(Screen):
                 self.int_digitos = int(self.digitos.text)
         except:
             self.int_digitos = 3
-            self.texto_ayuda.text = f"Sumandos: {self.int_sumandos} Digitos_error: {self.int_digitos}"
+            #self.texto_ayuda.text = f"Sumandos: {self.int_sumandos} Digitos_error: {self.int_digitos}"
 
         self.sumandos.text = ""
         self.digitos.text = ""
